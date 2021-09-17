eada40c369e5d853010836ad64a716004befdda5
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-17 10:00:25 -0400

    appended the count to the timer

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,11 +1,16 @@
 const startBtn = document.querySelector(".Start");
 const stopBtn = document.querySelector(".Stop");
 startBtn.addEventListener("click", () => {
-const time = setInterval(countTimer(36000), 1);
+const time = setInterval(countTimer(36000), 1000);
 })
 stopBtn.addEventListener("click", () => {
     clearInterval(time);
 })
+const timer = document.querySelector(".timer");
+const body = document.querySelector("body");
 function countTimer (count) {
     console.log(count);
+    count++;
+    timer.textContent = count;
+    body.append(timer);
 }
